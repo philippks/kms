347efef0110ce5bd66b6cc881fdb1e05e0f13493
Philipp <philippks@protonmail.com>
2019-07-05 08:59:40 +0200

    fix failing ci

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,14 +6,9 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless disable-gpu) },
-    loggingPrefs: { browser: 'ALL' }
-  )
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
 
-  Capybara::Selenium::Driver.new app,
-                                 browser: :chrome,
-                                 desired_capabilities: capabilities
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.javascript_driver = :headless_chrome
