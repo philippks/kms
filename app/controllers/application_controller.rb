245248afd7db277c4444b1a175969cc7c590c7c5
Philipp <philippks@protonmail.com>
2019-10-06 12:22:49 +0200

    set sentry context only if user logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,10 @@ class ApplicationController < ActionController::Base
   private
 
   def set_raven_context
-    Raven.user_context(id: current_user.id, username: current_user.name, email: current_user.email)
+    if current_user.present?
+      Raven.user_context(id: current_user.id, username: current_user.name, email: current_user.email)
+    end
+
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
