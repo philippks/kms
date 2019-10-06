c7d52822f43329f963c29cc49bb4515b457d7a4f
Philipp <philippks@protonmail.com>
2019-10-06 13:29:28 +0200

    rails app:upgrade

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,14 @@ Bundler.require(*Rails.groups)
 
 module Kms
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
     # configure global here, since it is used in {environmnet}.rb
     Global.configure do |config|
       config.environment = Rails.env.to_s
