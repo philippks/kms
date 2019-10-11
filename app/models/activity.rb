08f67a61c74885b5a1069b192847a64872b299e0
Philipp <philippks@protonmail.com>
2019-10-11 10:16:02 +0200

    :nail_care:

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,9 +4,9 @@ class Activity < Effort
   validates :hours, :hourly_rate, presence: true
   validates :hours, numericality: { greater_than_or_equal: 0 }
 
-  scope :for_activity_category, -> (activity_category_id) do
+  scope :for_activity_category, lambda { |activity_category_id|
     where(activity_category_id: activity_category_id)
-  end
+  }
 
   monetize :hourly_rate_cents, :amount_cents
 
