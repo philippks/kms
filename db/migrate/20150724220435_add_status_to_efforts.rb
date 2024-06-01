1fd6e908e519752245b0371e861b0e2fadc63481
Philipp <8806985+philippks@users.noreply.github.com>
2024-06-01 19:15:11 +0200

    Delete all migrations
    The migrations are very outdated and do not work anymore. To save the
    time fixing them, I decided to delete them.

--- db/migrate/20150724220435_add_status_to_efforts.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusToEfforts < ActiveRecord::Migration
-  def change
-    add_column :activities, :state, :integer, default: 0
-    add_column :expenses, :state, :integer, default: 0
-  end
-end
--- db/migrate/20170627141518_remove_active_from_employees.rb
+++
@@ -1,5 +0,0 @@
-class RemoveActiveFromEmployees < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :employees, :active
-  end
-end
