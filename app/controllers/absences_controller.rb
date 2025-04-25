1ceb5a41ab371f6d42e12369392f426967e92e33
Philipp Koster <koster@puzzle.ch>
2025-04-25 10:03:02 +0200

    propose absence hours

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -16,13 +16,10 @@ class AbsencesController < ApplicationController
     @absence.reason = Absence.reason.find_value(params['reason'].to_i) if params['reason']
     @absence.from_date = Date.current
     @absence.to_date = Date.current
+    @absence.hours = AbsencesService.new.calculate_default_hours(@absence.from_date, @absence.to_date, current_employee)
   end
 
   def create
-    if @absence.hours.nil? 
-      @absence.hours = AbsencesService.new.calculate_default_hours(@absence)
-    end
-
     @absence.save
     respond_with @absence, location: absences_path
   end
@@ -37,9 +34,22 @@ class AbsencesController < ApplicationController
     respond_with @absence
   end
 
+  def default_hours
+    from_date = Date.parse(default_hours_params[:from_date])
+    to_date = Date.parse(default_hours_params[:to_date])
+
+    default_hours = AbsencesService.new.calculate_default_hours(from_date, to_date, current_employee)
+
+    render json: { default_hours: }
+  end
+
   private
 
   def absence_params
     params.require(:absence).permit :employee_id, :text, :date, :hours, :from_date, :to_date, :reason
+  end
+
+  def default_hours_params
+    params.require(:absence).permit :from_date, :to_date
   end
 end
--- app/services/absences_service.rb
+++ app/services/absences_service.rb
@@ -1,11 +1,11 @@
 class AbsencesService
-  def calculate_default_hours(absence)
-    if absence.from_date.present? && absence.to_date.present?
-      if absence.from_date.monday? && absence.to_date.friday?
-        TargetHours.hours_between_for_employee(from: absence.from_date, to: absence.to_date, employee: absence.employee)
-      else
-        TargetHours.hours_between(from: absence.from_date, to: absence.to_date)
-      end
+  def calculate_default_hours(from_date, to_date, employee)
+    return nil if to_date < from_date
+
+    if from_date.monday? && to_date.friday?
+      TargetHours.hours_between_for_employee(from: from_date, to: to_date, employee:)
+    else
+      TargetHours.hours_between(from: from_date, to: to_date)
     end
   end
 end
